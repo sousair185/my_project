c9c0fe58d06f476dd66e8cafb1e080eb9224932b
sousair185 <igodrisousa@outlook.com>
2022-06-24 15:29:34 -0300

    bug: vercel uploading

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Content } from "./style";
+import {
+  FaLinkedin,
+  FaFacebook,
+  FaInstagram,
+  FaEnvelope,
+} from "react-icons/fa";
+import ItemContact from "../../../components/Header/ItemContact";
+
+export default function Contact() {
+  return (
+    <Container>
+      <Content>
+        <ItemContact
+          IconFa={FaLinkedin}
+          LinkContact="https://www.linkedin.com/in/sousa-igor"
+        />
+        <ItemContact
+          IconFa={FaFacebook}
+          LinkContact="https://www.facebook.com/sousair"
+        />
+        <ItemContact
+          IconFa={FaInstagram}
+          LinkContact="https://www.instagram.com/igorsousa185"
+        />
+        <ItemContact IconFa={FaEnvelope} LinkContact="igodrisousa@gmail.com" />
+      </Content>
+    </Container>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import { FaArrowRight } from "react-icons/fa";
+import {
+  Container,
+  Content,
+  Infos,
+  Name,
+  Function,
+  Intro,
+  LinkProjects,
+  Logo,
+  Img,
+} from "../../styles/indexStyle";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <Container>
+      <Content>
+        <Infos>
+          <Name>Olá, me chamo Igor Sousa</Name>
+          <Function>Desenvolvedor Fullstack</Function>
+          <Intro>Atualmente estudando Backend</Intro>
+          <Img className="gif" src="/images/mario.gif" alt="mario" />
+          <Link href="projects">
+            <LinkProjects className="glow-on-hover" type="button">
+              PROJETOS <FaArrowRight />
+            </LinkProjects>
+          </Link>
+        </Infos>
+        <Logo>
+          <Img src="/images/logo.png" alt="logo" />
+        </Logo>
+      </Content>
+    </Container>
+  );
+}
