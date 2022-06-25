70798c56b78da1931ed8cc7a9ca0d7f8f54e8290
sousair185 <igodrisousa@outlook.com>
2022-06-25 06:54:34 -0300

    fix: next build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dc8353b303d5f26f"; }
+/******/ 		__webpack_require__.h = function() { return "39b3ec9801309e57"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
